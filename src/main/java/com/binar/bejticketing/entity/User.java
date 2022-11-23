dbf71033bf2aee84970a70e6b8b1cff8e8a712e3
Fathan azka <fazka313@gmail.com>
2022-11-23 23:19:35 +0700

    migrate to railway config

--- src/main/java/com/binar/bejticketing/entity/User.java
+++ src/main/java/com/binar/bejticketing/entity/User.java
@@ -29,6 +29,12 @@ public class User {
     @Column(name = "last_name")
     private String lastName;
 
+    @Column(name = "birthday")
+    private String birthday;
+
+    @Column(name = "address")
+    private String address;
+
     @Column(name = "email")
     private String email;
 
