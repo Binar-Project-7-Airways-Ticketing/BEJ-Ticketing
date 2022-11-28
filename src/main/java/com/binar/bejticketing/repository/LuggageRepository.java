1caf0c3de451a8f0adc2f8ba861bdec080e01d5b
Fathan azka <fazka313@gmail.com>
2022-11-28 13:54:35 +0700

    adding implementation of Luggage

--- src/main/java/com/binar/bejticketing/repository/LuggageRepository.java
+++ src/main/java/com/binar/bejticketing/repository/LuggageRepository.java
@@ -2,6 +2,18 @@ package com.binar.bejticketing.repository;
 
 import com.binar.bejticketing.entity.Luggage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface LuggageRepository extends JpaRepository<Luggage, Long> {
+    @Query("SELECT l FROM Luggage l WHERE l.isReady = :state")
+    List<Luggage> findLuggageByStateTrue(boolean state);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Luggage l SET l.isReady = false WHERE l.idLuggage = :idLuggage")
+    Luggage updateLuggage(Long idLuggage);
 }
--- src/main/java/com/binar/bejticketing/service/LuggageService.java
+++ src/main/java/com/binar/bejticketing/service/LuggageService.java
@@ -1,7 +1,15 @@
 package com.binar.bejticketing.service;
 
+import com.binar.bejticketing.entity.Luggage;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface LuggageService {
+    List<Luggage> getAllLuggage();
+    List<Luggage> getLuggageByState(boolean state);
+
+    Luggage createLuggage(Luggage luggage);
+    Luggage updateStateLuggage(Long id);
 }
