350d34810489dd9da11a76de12324435131c6593
Fathan azka <fazka313@gmail.com>
2022-12-13 15:26:55 +0700

    added updatePlaneDetail in Luggage

--- src/main/java/com/binar/bejticketing/service/order/LuggageServiceImpl.java
+++ src/main/java/com/binar/bejticketing/service/order/LuggageServiceImpl.java
@@ -1,17 +1,23 @@
 package com.binar.bejticketing.service.order;
 
 import com.binar.bejticketing.entity.Luggage;
+import com.binar.bejticketing.entity.PlaneDetails;
 import com.binar.bejticketing.repository.LuggageRepository;
+import com.binar.bejticketing.repository.PlaneDetailsRepository;
 import com.binar.bejticketing.service.LuggageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LuggageServiceImpl implements LuggageService {
     @Autowired
     private LuggageRepository luggageRepository;
+    @Autowired
+    private PlaneDetailsRepository planeDetailsRepository;
     @Override
     public List<Luggage> getAllLuggage() {
         return luggageRepository.findAll();
@@ -35,5 +41,18 @@ public class LuggageServiceImpl implements LuggageService {
     @Override
     public Luggage updateStateLuggage(Long id) {
         return luggageRepository.updateLuggage(id);
+    }
+
+    @Override
+    public Luggage updatePlaneDetailLuggage(Long idLuggage, Long idPlane) {
+        Optional<PlaneDetails> planeDetails = planeDetailsRepository.findById(idPlane);
+        Optional<Luggage> luggageCheck = luggageRepository.findById(idLuggage);
+
+
+        if (planeDetails.isEmpty()){
+            throw new EntityNotFoundException();
+        }
+        luggageCheck.get().setPlaneDetails(planeDetails.get());
+        return luggageCheck.get();
     }
 }
