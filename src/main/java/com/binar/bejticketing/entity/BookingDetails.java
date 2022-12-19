f28b0861e8495a5efcb64d4c84b5d6194e4c51a2
Fathan azka <fazka313@gmail.com>
2022-12-19 21:00:43 +0700

    change to One to many in Booking

--- src/main/java/com/binar/bejticketing/entity/BookingDetails.java
+++ src/main/java/com/binar/bejticketing/entity/BookingDetails.java
@@ -28,8 +28,8 @@ public class BookingDetails {
     @Column(name = "state_pricing")
     private boolean statePricing = false;
 
-    @ManyToOne
-    @JoinColumn(name = "id_booking")
+    @OneToOne(mappedBy = "bookingDetails")
+    @JoinColumn(name = "id_booking", referencedColumnName = "id_booking")
     @JsonIgnore
     private Booking booking;
 
