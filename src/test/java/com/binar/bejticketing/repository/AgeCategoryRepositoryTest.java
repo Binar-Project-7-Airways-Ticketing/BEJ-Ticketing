13801a3c3867a15f599f0dc99882552692b632df
Fathan azka <fazka313@gmail.com>
2022-12-30 10:43:36 +0700

    remove idbooking in getHistory

--- src/test/java/com/binar/bejticketing/repository/AgeCategoryRepositoryTest.java
+++ src/test/java/com/binar/bejticketing/repository/AgeCategoryRepositoryTest.java
@@ -2,9 +2,13 @@ package com.binar.bejticketing.repository;
 
 import com.binar.bejticketing.entity.AgeCategory;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigInteger;
 import java.text.ParseException;
@@ -16,17 +20,16 @@ import static com.binar.bejticketing.utils.AgeCategoryName.CHILDREN;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
+@RunWith(SpringRunner.class)
 class AgeCategoryRepositoryTest {
     @Autowired
     private AgeCategoryRepository ageCategoryRepository;
+    @Autowired
+    private TestEntityManager entityManager;
     private AgeCategory ageCategory;
-    @AfterEach
-    void tearDown() {
-        ageCategoryRepository.deleteAll();
-    }
+    @BeforeEach
+    void setUp(){
 
-    @Test
-    void updateAgeCategory() {
         Date parse = null;
         try {
             parse = new SimpleDateFormat("dd-MM-yyyy hh:MM:ss").parse("12-07-2001 12:00:00");
@@ -34,8 +37,19 @@ class AgeCategoryRepositoryTest {
             throw new RuntimeException(e);
         }
         ageCategory = AgeCategory.builder().idCategory(2L).nameCategory(CHILDREN).price(BigInteger.valueOf(10000)).createdAt(parse).updatedAt(null).build();
-        ageCategoryRepository.save(ageCategory);
-        AgeCategory ageCategory1 = ageCategoryRepository.updateAgeCategory(2L,"ADULT");
-        assertThat(ageCategory1.getIdCategory()).isEqualTo(2L);
+//        ageCategoryRepository.saveAndFlush(ageCategory);
+        entityManager.persist(ageCategory);
+        entityManager.flush();
+    }
+    @AfterEach
+    void tearDown() {
+        ageCategoryRepository.deleteAll();
+    }
+
+    @Test
+    void updateAgeCategory() {
+        AgeCategory found = ageCategoryRepository.updateAgeCategory(2L,"ADULT");
+        assertThat(found.getNameCategory()).isEqualTo(ageCategory.getNameCategory());
+//        assertThat(ageCategory1.getIdCategory()).isEqualTo(2L);
     }
 }
--- src/test/java/com/binar/bejticketing/repository/BookingRepositoryTest.java
+++ src/test/java/com/binar/bejticketing/repository/BookingRepositoryTest.java
@@ -1,11 +1,15 @@
 package com.binar.bejticketing.repository;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@DataJpaTest
 class BookingRepositoryTest {
 
+    @Autowired BookingRepository bookingRepository;
+    @Autowired BookingDetailsRepository bookingDetailsRepository;
     @Test
     void updateBookingValid() {
     }
